a6c90cf0f9277f5837c5c57515a7aaa0cefae774
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-02-05 16:43:31 +0900

    初期設定とshadcnの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="flex flex-col min-h-scree container items-center justify-between p-24 mx-auto">
+      <Button>hello shadcn</Button>
+    </main>
+  );
 }
