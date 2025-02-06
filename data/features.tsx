a3de22ce9006734fd5f2c45fce83074a7d0c7b7a
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-02-06 18:33:20 +0900

    文章の変更

--- data/features.tsx
+++ data/features.tsx
@@ -10,20 +10,20 @@ interface Feature {
 export const features: Feature[] = [
   {
     icon: <CreditCard className="w-8 h-8 text-blue-500" />,
-    title: "かんたんキャッシュレス決済",
+    title: "海外から国内にかけて利用できる決済",
     description:
-      "Stripeを利用した安全な決済システムで、クレジットカードからQR決済まで幅広く対応",
+      "Stripeを利用した安全な決済システムで、クレジットカードからQR決済まで幅広く対応。",
   },
   {
     icon: <Package className="w-8 h-8 text-blue-500" />,
-    title: "在庫管理もラクラク",
+    title: "利用開始まで最短1日",
     description:
-      "イベント中の在庫状況をリアルタイムで把握、スマホで簡単に更新可能",
+      "豊富なテンプレートを用意しており、最短即日で利用開始が可能。展示会やイベント販売にも迅速に対応。",
   },
   {
     icon: <ShoppingBag className="w-8 h-8 text-blue-500" />,
     title: "オンライン販売も対応",
     description:
-      "イベント前の予約販売や、イベント後のフォローアップセールスもカバー",
+      "展示会やイベントで利用した後もECサイトとして継続利用可能。オンライン販売へスムーズに移行。",
   },
 ];
