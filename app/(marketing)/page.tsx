fa8961c895de026cbd90b9708cd2bd5aebf898d5
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-02-14 15:14:09 +0900

    idの追加とリファクタリング

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -152,7 +152,7 @@ const LandingPage = () => {
       </section>
 
       {/* 5. 料金プラン */}
-      <section className="py-24 bg-gray-50">
+      <section id="fee" className="py-24 bg-gray-50">
         <div className="container mx-auto px-4 sm:px-6">
           <h2 className="text-3xl font-bold text-center mb-6">
             シンプルな料金体系
