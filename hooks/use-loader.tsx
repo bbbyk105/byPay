b5d3e77a3a0f09f1c981f33425231ac5f6410345
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-02-25 23:00:30 +0900

    謎変更

---
+++ hooks/use-loader.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export const useLoader = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading;
+};
